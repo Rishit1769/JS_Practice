b89774ccbfd87ccf9bacd68ce93f85c754bd35b0
Rishit <singhrishit471@gmail.com>
2026-03-24 20:37:41 +0530

    DOM Q3

---
+++ DOM-exercise/index.js
@@ -0,0 +1,6 @@
+let order = document.getElementById("coffeeOrder")
+let btn = document.getElementById("changeOrder")
+
+btn.addEventListener('click',function(){
+    order.textContent = "Espresso"
+})
